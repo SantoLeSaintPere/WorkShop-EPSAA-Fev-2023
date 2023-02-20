846ee3c4b055d8abcad0386f4bbfb928ffe86873
SantoLeSaintPere <72297300+SantoLeSaintPere@users.noreply.github.com>
2023-02-20 15:08:57 +0100

    Movement + camera follow

--- Gaming-UNITY-PROJECT/Assets/FollowPlayer.cs
+++ Gaming-UNITY-PROJECT/Assets/FollowPlayer.cs
@@ -6,15 +6,20 @@ public class FollowPlayer : MonoBehaviour
 {
     public Transform target;
     public Vector3 offest;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         transform.position = target.position + offest;
+    }
+    private void FixedUpdate()
+    {
+        FollowSmooth();
+    }
+    void FollowSmooth()
+    {
+        Vector3 targetPosition = target.position;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition + offest, speed * Time.fixedDeltaTime);
+        transform.position = smoothedPosition;
     }
 }
--- Gaming-UNITY-PROJECT/Assets/SCRIPT/PlayerMove.cs
+++ Gaming-UNITY-PROJECT/Assets/SCRIPT/PlayerMove.cs
@@ -4,14 +4,7 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    private float speed = 2;
-    public GameObject bidule;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        bidule.SetActive(false);
-    }
+    public float speed = 2;
 
     // Update is called once per frame
     void Update()
@@ -23,13 +16,27 @@ public class PlayerMove : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Z))
         {
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
-            bidule.SetActive(true);
+            transform.Translate(Vector3.forward.normalized * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back.normalized * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Translate(Vector3.left.normalized * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right.normalized * speed * Time.deltaTime);
         }
 
         else
         {
-            bidule.SetActive(false);
+            transform.Translate(Vector3.zero);
         }
     }
 }
